cced2832e90d2f1165c2e049392df7c27ef648df
stellaf <stella.fredo@gmail.com>
2017-04-26 11:19:58 +0200

    Add files via upload

---
+++ mro_order_duration/__openerp__.py
@@ -0,0 +1,39 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo
+#    Copyright (C) 2013-2016 Stella Fredö.
+#
+##############################################################################
+
+{
+    'name': 'MRO_order_duration',
+    'version': '10.1.0.1',
+    'summary': 'Asset Maintenance order calendar drag',
+    'description': """
+Make the maintenance order can be drag on the calendar.
+=====================================
+
+Asset Maintenance, Repair and Operation.
+
+Main Features
+-------------
+    * add field duration for asset Maintenance order
+    * so that the maintenance order time duration can be drag on the calendar
+
+Required modules:
+    * asset
+    * mro 
+    """,
+    'author': 'Stella Fredö',
+    'website': '',
+    'category': 'Industries',
+    'sequence': 0,
+    'depends': ['asset','mro'],
+    'data': [
+       'mro_view.xml',
+   ],
+    'installable': True,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ mro_order_duration/mro.py
@@ -0,0 +1,21 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo
+#    Copyright (C) 2013-2017 CodUP (<http://codup.com>).
+#
+##############################################################################
+
+import time
+from odoo import api, fields, models, _
+import odoo.addons.decimal_precision as dp
+
+
+class mro_order(models.Model):
+    """
+    add duration on Maintenance Orders, so that we can drag calendar
+    """
+    _inherit = 'mro.order'
+    order_duration = fields.Float(string='Duration', store=True, default=1.0)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
